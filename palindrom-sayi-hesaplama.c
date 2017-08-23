3b787a9f7d8980e167cae92429b0ebf3f8609e00
muhammed konyar <muhammedkonyar04@gmail.com>
2017-08-23 20:04:45 +0300

    palindrom sayi hesaplama kodlarında format düzenlendi

--- palindrom-sayi-hesaplama.c
+++ palindrom-sayi-hesaplama.c
@@ -2,19 +2,19 @@
 #include <conio.h>
 main()
 {
-int a, b, c, d, i, sayac = 0;
-for(i=1000;i<=9999;i++)
-{
-a = i / 1000;
-b = (i%1000) / 100;
-c = (i%100) / 10;
-d = i%10;
-if(a == d && b == c)
-{
-printf(“%5d”, i);
-sayac ++;
-}
-}
-printf(“\n\n4 Basamakli Palindrom Sayi Adeti = %d”, sayac);
-getche();
+	int a, b, c, d, i, sayac = 0;
+	for(i=1000;i<=9999;i++)
+	{
+		a = i / 1000;
+		b = (i%1000) / 100;
+		c = (i%100) / 10;
+		d = i%10;
+		if(a == d && b == c)
+		{
+			printf(“%5d”, i);
+			sayac ++;
+		}
+	}
+	printf(“\n\n4 Basamakli Palindrom Sayi Adeti = %d”, sayac);
+	getche();
 }
